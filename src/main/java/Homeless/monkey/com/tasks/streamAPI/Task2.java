22854d2fcf05071155483e8f64a49c8a5feef4a0
HomelessMonkey <adikovwork@gmail.com>
2026-03-05 03:07:00 +0300

    Stream API Task2

---
+++ src/main/java/Homeless/monkey/com/tasks/streamAPI/Task2.java
@@ -0,0 +1,41 @@
+package Homeless.monkey.com.tasks.streamAPI;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Task2 {
+    public static void main(String[] args) {
+        List<Student> students = Arrays.asList(
+                new Student("Student1", Map.of("Math", 90, "Physics", 85)),
+                new Student("Student2", Map.of("Math", 95, "Physics", 88)),
+                new Student("Student3", Map.of("Math", 88, "Chemistry", 92)),
+                new Student("Student4", Map.of("Physics", 78, "Chemistry", 85))
+        );
+
+        students.parallelStream()
+                .flatMap(s-> s.getGrades().entrySet().stream())
+                .collect(Collectors.groupingBy(
+                        Map.Entry::getKey,
+                        Collectors.averagingDouble(Map.Entry::getValue)))
+                .forEach((k, v)-> System.out.printf("%s %.2f%n",k,v));
+    }
+}
+
+class Student {
+    private String name;
+    private Map<String, Integer> grades;
+
+    public Student(String name, Map<String, Integer> grades) {
+        this.name = name;
+        this.grades = grades;
+    }
+
+    public Map<String, Integer> getGrades() {
+        return grades;
+    }
+}
+
+
+
