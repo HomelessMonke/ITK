d744046f0ad16dc2e62e89d4d445b21c17f69414
HomelessMonkey <adikovwork@gmail.com>
2026-03-05 05:16:14 +0300

    Stream API Task3

---
+++ src/main/java/Homeless/monkey/com/tasks/streamAPI/Task3.java
@@ -0,0 +1,16 @@
+package Homeless.monkey.com.tasks.streamAPI;
+
+import java.util.concurrent.ForkJoinPool;
+
+public class Task3 {
+    public static void main(String[] args) {
+        int n = 10;
+
+        ForkJoinPool forkJoinPool = new ForkJoinPool();
+        FactorialTask factorialTask = new FactorialTask(n);
+
+        long result = forkJoinPool.invoke(factorialTask);
+
+        System.out.println("Факториал " + n + "! = " + result);
+    }
+}
