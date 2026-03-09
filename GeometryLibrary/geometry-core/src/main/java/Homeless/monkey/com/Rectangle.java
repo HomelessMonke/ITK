82e4f9ee0075d811cecb3ab73dce924e955d1cb8
HomelessMonkey <adikovwork@gmail.com>
2026-03-09 07:27:16 +0300

    GeometryLibrary 1.0.0 install

---
+++ GeometryLibrary/geometry-core/src/main/java/Homeless/monkey/com/Rectangle.java
@@ -0,0 +1,15 @@
+package Homeless.monkey.com;
+
+public class Rectangle implements Shape{
+    @Override
+    public double area() {
+        System.out.println("Площадь прямоугольника");
+        return 0;
+    }
+
+    @Override
+    public double perimeter() {
+        System.out.println("Периметр прямоугольника");
+        return 0;
+    }
+}
