c825a5789f3b8252035d7dff3e68ee3d5df26111
HomelessMonkey <adikovwork@gmail.com>
2026-03-09 07:56:56 +0300

    GeometryApp + зависимость на geometry-core

---
+++ GeometryApp/src/main/java/Homeless/monkey/com/Main.java
@@ -0,0 +1,17 @@
+package Homeless.monkey.com;
+
+public class Main {
+    public static void main(String[] args) {
+        Circle circle = new Circle();
+        circle.area();
+        circle.perimeter();
+
+        Rectangle rect = new Rectangle();
+        rect.area();
+        rect.perimeter();
+
+        Triangle triangle = new Triangle();
+        triangle.area();
+        triangle.perimeter();
+    }
+}
